3513a6f7be35feee8ddc3354ca0f3884d0edffff
Admir Trakic <atrakic@users.noreply.github.com>
2024-12-04 12:52:00 +0100

    Try to deploy a static website

---
+++ deploy-static-website.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e -o pipefail
+
+# Deploy a static website
+
+# helm list -A
+# helm -n crossplane-system get values crossplane
+# helm -n localstack-system get values localstack
+
+#kubectl get providers
+kubectl get providers provider-aws-s3
+kubectl get buckets -n crossplane-system
+kubectl apply -f s3-website-bucket.yaml
+
+# https://docs.aws.amazon.com/cli/v1/userguide/cli-configure-endpoints.html
+kubectl get secret localstack-aws-token -n crossplane-system  -o jsonpath="{.data.credentials}" | base64 -d > .aws
+export AWS_CONFIG_FILE=.aws
+aws_args=( --endpoint-url http://localhost:4567 )
+aws s3 ls "${aws_args[@]}"
+aws s3 sync "${aws_args[@]}" ./website/ s3://crossplane-s3-bucket --acl public-read
